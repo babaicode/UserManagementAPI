6043c400d01d1eac0d3315f10b19259dcb41b945
Andrei Babai <amaracheuski@mountainlift.cz>
2023-11-29 21:57:21 +0100

    try to fix eslint

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,16 +1,20 @@
 module.exports = {
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier',
-        'prettier/@typescript-eslint',
-      ],
-      parser: '@typescript-eslint/parser',
-      plugins: ['@typescript-eslint'],
-      parserOptions: {
-        ecmaVersion: 2021,
-        sourceType: 'module',
-      },
-      rules: {
-      },
-}
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {
+  },
+};
